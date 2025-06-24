6f30758178ee6bdfdacfd3d814d5553df7956d7d
veera-ops <106005430+veera-ops@users.noreply.github.com>
2025-06-24 12:02:17 +0530

    Create  HelloWorldTest.java

---
+++ HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testGetMessage() {
+        assertEquals("Hello, World!", HelloWorld.getMessage());
+    }
+}
