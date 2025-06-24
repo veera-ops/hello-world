5c6b9ffcc85dd632a66a8281042903f1b1f268aa
veera-ops <106005430+veera-ops@users.noreply.github.com>
2025-06-24 12:01:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+    public static String getMessage() {
+        return "Hello, World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(getMessage());
     }
 }
